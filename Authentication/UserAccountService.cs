2cf6fcabdb5c8943b6ad019ce63fe8f8e6dd0e78
Bonifasius Edwin Subagyo <boniedwin@gmail.com>
2022-06-23 09:32:19 +0700

    fix function

--- Authentication/UserAccountService.cs
+++ Authentication/UserAccountService.cs
@@ -4,8 +4,11 @@ using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace TSJ_CRI.Authentication
@@ -19,57 +22,78 @@ namespace TSJ_CRI.Authentication
             ConnStrProd = config.GetConnectionString("103ConnTest");
         }
 
-        public async Task<(IEnumerable<UserAccount>, string, string)> GetUser(string _username, string _password)
+        public async Task<(IEnumerable<UserAccount>, string)> GetUser(string _username, string _password)
         {
-            var _WindowsAuth = WindowsAuth(_username, _password);
-            if (_WindowsAuth == null)
-            {
-                try
-                {
-                    var sql = "SELECT username_window username, price_level role, '3200' cabang " +
-                            "FROM PRICE_USERS " +
-                            "where username_window = @username";
-                    var conn = new SqlConnection(ConnStrProd);
-                    var parameters = new { username = _username };
-                    var user = await conn.QueryAsync<UserAccount>(sql, parameters);
-                    return (user, null, null);
-                }
-                catch (Exception ex)
-                {
-                    return (null, null, ex.Message.ToString());
-                }
-            }
-            else
-            {
-                return (null, _WindowsAuth.ToString(), null);
-            }
-        }
-
-        private static string WindowsAuth(string _username, string _password)
-        {
-            string path = "LDAP://inf-addc-01.enseval.com";
             try
             {
-                using System.DirectoryServices.DirectoryEntry entry = new(path, _username, _password);
-                using DirectorySearcher searcher = new(entry);
-                searcher.Filter = "(samaccountname=" + _username + ")";
-                var result = searcher.FindOne();
-
-                //ResultPropertyCollection fields = result.Properties;
-                //foreach (String ldapField in fields.PropertyNames)
-                //{
-                //    foreach (Object myCollection in fields[ldapField])
-                //    {
-                //        //if (ldapField == "department")
-                //        //    role = myCollection.ToString().ToLower();
-                //    }
-                //}
-                return null;
+                var sql = "SELECT username_window username, price_level role, '3200' cabang " +
+                        "FROM PRICE_USERS " +
+                        "where username_window = @username";
+                var conn = new SqlConnection(ConnStrProd);
+                var parameters = new { username = _username };
+                var user = await conn.QueryAsync<UserAccount>(sql, parameters);
+                return (user, null);
             }
             catch (Exception ex)
             {
-                return ex.Message.ToString();
+                return (null, ex.Message.ToString());
             }
+        }
+
+        public string WindowsAuth(string _username, string _password)
+        {
+            string path = "inf-addc-01.enseval.com";
+            try
+            {
+                LdapConnection connection = new(path);
+                NetworkCredential credential = new(_username, _password);
+                connection.Credential = credential;
+                connection.Bind();
+                return null;
+            }
+            catch (LdapException lexc)
+            {
+                String ServerMessage = lexc.ServerErrorMessage;
+
+                if (ServerMessage != null)
+                {
+                    string errorHex = ServerMessage.Substring(ServerMessage.IndexOf(", data") + 7, 3);
+                    var errorCode = Convert.ToInt64(errorHex, 16);
+                    var errorDesc = new Win32Exception((int)errorCode).Message;
+                    return errorDesc.ToString();
+                }
+                else
+                {
+                    return lexc.Message.ToString();
+                }
+            }
+            catch (Exception exc)
+            {
+                return exc.Message.ToString();
+            }
+
+            //try
+            //{
+            //    using System.DirectoryServices.DirectoryEntry entry = new(path, _username, _password);
+            //    using DirectorySearcher searcher = new(entry);
+            //    searcher.Filter = "(samaccountname=" + _username + ")";
+            //    var result = searcher.FindOne();
+
+            //    //ResultPropertyCollection fields = result.Properties;
+            //    //foreach (String ldapField in fields.PropertyNames)
+            //    //{
+            //    //    foreach (Object myCollection in fields[ldapField])
+            //    //    {
+            //    //        //if (ldapField == "department")
+            //    //        //    role = myCollection.ToString().ToLower();
+            //    //    }
+            //    //}
+            //    return null;
+            //}
+            //catch (Exception ex)
+            //{
+            //    return ex.Message.ToString();
+            //}
         }
     }
 }
