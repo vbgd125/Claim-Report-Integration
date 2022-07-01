e9ea64d19b4007a48d43d3ab398897d8b3d7392a
Kevin Widjaja <kevin.widjaja@binus.ac.id>
2022-07-01 10:26:13 +0700

    date and bug

---
+++ wwwroot/custom_assets/js/datepicker.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+
+    // INITIALIZE DATEPICKER PLUGIN
+    $('.datepicker').datepicker({
+        clearBtn: true,
+        format: "dd/mm/yyyy"
+    });
+
+
+    // FOR DEMO PURPOSE
+    $('#reservationDate').on('change', function () {
+        var pickedDate = $('input').val();
+        $('#pickedDate').html(pickedDate);
+    });
+});
