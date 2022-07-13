97f4cf2a4b294577c9d18a0ec7b3ee0ce7c3fa55
Kevin Widjaja <kevin.widjaja@binus.ac.id>
2022-07-13 14:23:38 +0700

    add user

--- Model/UserManage.cs
+++ Model/UserManage.cs
@@ -21,12 +21,15 @@ namespace TSJ_CRI.Model
 
     public class UserNew
     {
-        [Required(ErrorMessage = "Wajib Pilih cabang")]
-        public string org_id { get; set; }
         
+        public string org_id { get; set; }
+
+        [Required(ErrorMessage = "Username tidak dapat kosong")]
+        [MaxLength(100, ErrorMessage = "Panjang maksimum tidak lebih dari 100 karakter")]
+        [MinLength(5, ErrorMessage = "panjang maksimum minimal 6 karakter")]
         public string username { get; set; }
-        [Required(ErrorMessage = "Wajib pilih roles")]
         public string roles { get; set; }
+        [Required(ErrorMessage = "Email tidak dapat kosong")]
         public string email { get; set; }
     }
 
